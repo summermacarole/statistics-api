d735611b5a3c7bbbac8122b18a6a2d469a007b04
macarole <497031691@qq.com>
2018-05-31 10:24:04 +0800

    添加cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Koa = require('koa')
 const body = require('koa-body')
+const cors = require('@koa/cors')
 const app = new Koa();
 const router = require('./router')
 
 
-
+app.use(cors())
 app.use(body())
 app.use(router.routes(), router.allowedMethods())
 app.use(async ctx => {
--- router/other.js
+++ router/other.js
@@ -6,32 +6,39 @@ other.get('/', async (ctx, next) => {
 })
 other.get('/getUserInfo', async (ctx, next) => {
     let result = {
-        schoolId: 5,
-        loginName: 'macarole'
+        "userId": "26727",
+        "logonName": "admin",
+        "userName": "学海教育",
+        "userType": "8",
+        "schoolId": "3638",
+        "isStaff": false,
+        "depId": [],
+        "depTag": [],
+        "depName": [],
+        "verifyPhone": true,
+        "schoolName": "舟山市普陀第二中学"
     }
-    ctx.type = 'json'
+    ctx.type = 'application/json; charset=utf-8'
+    // ctx.set('name','macarole')
+    // ctx.set('Access-Control-Allow-Origin','*')
     ctx.body = result;
 })
 other.get('/getSchoolPower', async (ctx, next) => {
-    let result = [],
+    let result = {},
         count = 10;
     for (let i = 0; i < count; i++) {
-        result.push({
-            id: (i + 1),
-            value: (i + 1),
-            label: 'school' + (i + 1)
-        })
+        result[(i + 1)] = 'school' + (i + 1)
     }
     ctx.type = 'json'
     ctx.body = result;
 })
-other.post('/transferInterface',async (ctx,next)=>{
-    let url=ctx.request.body.url;
-    url=url.replace(3000,8080)
-    ctx.status=301
+other.post('/transferInterface', async (ctx, next) => {
+    let url = ctx.request.body.url;
+    url = url.replace(3000, 8080)
+    ctx.status = 301
     ctx.redirect(url)
     console.log(ctx.request.body)
-    ctx.body="我是 一个中专接口"
+    ctx.body = "我是 一个中专接口"
 })
 
 module.exports = other;
